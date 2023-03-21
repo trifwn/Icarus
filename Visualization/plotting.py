0d1d86f1e051aed4ed517b2aee1d4717634227a6
trifwn <tryfonthem@gmail.com>
2023-03-21 20:20:48 +0200

    Plotting Update to use 2D Database

--- Visualization/plotting.py
+++ Visualization/plotting.py
@@ -2,35 +2,117 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 
+colors = ['r', 'k', 'b', 'g', 'c', 'm', 'y', 'r', 'k', 'b', 'g']
+markers = ['x', 'o', '.', "*"]
 
-def plotAeorCoeffs(data, siz,title):
 
-    fig, axs = plt.subplots(2, 2, figsize=siz)
-    fig.suptitle(f'{title}', fontsize=16)
+def plotAirfoil(data, airfoil, solvers='All', size=(10, 10)):
+    fig, axs = plt.subplots(2, 2, figsize=size)
+
+    fig.suptitle(f'NACA {airfoil[4:]} Aero Coefficients', fontsize=16)
     axs[0, 0].set_title('Cm vs AoA')
-    # axs[0, 0].set_xlabel('a')
     axs[0, 0].set_ylabel('Cm')
+
     axs[0, 1].set_title('Cd vs AoA')
-    axs[0, 1].set_xlabel('a')
+    axs[0, 1].set_xlabel('AoA')
     axs[0, 1].set_ylabel('Cd')
+
     axs[1, 0].set_title('Cl vs AoA')
-    axs[1, 0].set_xlabel('a')
+    axs[1, 0].set_xlabel('AoA')
     axs[1, 0].set_ylabel('Cl')
+
     axs[1, 1].set_title('Cl vs Cd')
     axs[1, 1].set_xlabel('Cd')
-    # axs[1, 1].set_ylabel('Cl')
-    for dat in data:
-        foo, style, label = dat
-        a, cl, cd, cm = foo.T
-        axs[0, 1].plot(a, cd, style, label=label)
-        axs[1, 0].plot(a, cl, style, label=label)
-        axs[1, 1].plot(cd, cl, style, label=label)
-        axs[0, 0].plot(a, cm, style, label=label)
-    axs[0, 1].legend()
-    axs[1, 0].legend()
-    axs[1, 1].legend()
-    axs[0, 0].legend()
+
+    if solvers == ['All']:
+        solvers = ["Xfoil", "Foil2Wake", "OpenFoam"]
+
+    for i, reynolds in enumerate(list(data[airfoil])):
+        for j, solver in enumerate(solvers):
+            try:
+                polar = data[airfoil][reynolds][solver]
+
+                aoa, cl, cd, cm = polar.T.values
+                c = colors[i]
+                m = markers[j]
+                style = f"{c}{m}-"
+                label = f"{airfoil}: {reynolds} - {solver}"
+                axs[0, 1].plot(aoa, cd, style, label=label,
+                               markersize=3, linewidth=1)
+                axs[1, 0].plot(aoa, cl, style, label=label,
+                               markersize=3, linewidth=1)
+                axs[1, 1].plot(cd, cl, style, label=label,
+                               markersize=3, linewidth=1)
+                axs[0, 0].plot(aoa, cm, style, label=label,
+                               markersize=3, linewidth=1)
+            except KeyError as solver:
+                print(f"Run Doesn't Exist: {airfoil},{reynolds},{solver}")
+
     fig.tight_layout()
+    if len(solvers) == 3:
+        per = -.85
+    elif len(solvers) == 2:
+        per = -.6
+    else:
+        per = -0.4
+    axs[0, 1].grid()
+    axs[1, 0].grid()
+    axs[1, 1].grid()
+    axs[0, 0].grid()
+
+    axs[1, 0].legend(bbox_to_anchor=(-0.1, per),  ncol=3,
+                     fancybox=True, loc='lower left')
+
+
+def plotReynolds(data, airfoil, reyn, solvers='All', size=(10, 10)):
+    fig, axs = plt.subplots(2, 2, figsize=size)
+    fig.suptitle(
+        f'NACA {airfoil[4:]}- Reynolds={reyn}\n Aero Coefficients', fontsize=16)
+    axs[0, 0].set_title('Cm vs AoA')
+    axs[0, 0].set_ylabel('Cm')
+
+    axs[0, 1].set_title('Cd vs AoA')
+    axs[0, 1].set_xlabel('AoA')
+    axs[0, 1].set_ylabel('Cd')
+
+    axs[1, 0].set_title('Cl vs AoA')
+    axs[1, 0].set_xlabel('AoA')
+    axs[1, 0].set_ylabel('Cl')
+
+    axs[1, 1].set_title('Cl vs Cd')
+    axs[1, 1].set_xlabel('Cd')
+
+    if solvers == ['All']:
+        solvers = ["Xfoil", "Foil2Wake", "OpenFoam"]
+
+        for j, solver in enumerate(solvers):
+            try:
+                polar = data[airfoil][reyn][solver]
+                aoa, cl, cd, cm = polar.T.values
+                c = colors[j]
+                m = markers[j]
+                style = f"{c}{m}-"
+                label = f"{airfoil}: {reyn} - {solver}"
+                axs[0, 1].plot(aoa, cd, style, label=label,
+                               markersize=3, linewidth=1)
+                axs[1, 0].plot(aoa, cl, style, label=label,
+                               markersize=3, linewidth=1)
+                axs[1, 1].plot(cd, cl, style, label=label,
+                               markersize=3, linewidth=1)
+                axs[0, 0].plot(aoa, cm, style, label=label,
+                               markersize=3, linewidth=1)
+            except KeyError as solver:
+                print(f"Run Doesn't Exist: {airfoil},{reyn},{solver}")
+
+    fig.tight_layout()
+
+    axs[0, 1].grid()
+    axs[1, 0].grid()
+    axs[1, 1].grid()
+    axs[0, 0].grid()
+
+    axs[1, 0].legend(bbox_to_anchor=(-0.1, -0.25),  ncol=3,
+                     fancybox=True, loc='lower left')
 
 
 def plotCP(angle):
